ad9d959a1cc94487b034a3d8108b0d6a83b77a52
pallavideore <tusharindalkar87@gmail.com>
2020-05-08 11:27:46 +0530

    first commit

---
+++ src/test/java/com/dockerimage/dockerimages/DockerImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerimage.dockerimages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
